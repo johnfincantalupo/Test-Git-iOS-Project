4f60c9c8cfd0b6955004e431eb4bc462ef20a34a
John Incantalupo <john.incantalupo@gmail.com>
2014-01-05 19:14:01 -0500

    added Test class

---
+++ JFITest.h
@@ -0,0 +1,13 @@
+//
+//  JFITest.h
+//  Git Test
+//
+//  Created by admin on 1/5/14.
+//  Copyright (c) 2014 Incantalupo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JFITest : NSObject
+
+@end
---
+++ JFITest.m
@@ -0,0 +1,13 @@
+//
+//  JFITest.m
+//  Git Test
+//
+//  Created by admin on 1/5/14.
+//  Copyright (c) 2014 Incantalupo. All rights reserved.
+//
+
+#import "JFITest.h"
+
+@implementation JFITest
+
+@end
